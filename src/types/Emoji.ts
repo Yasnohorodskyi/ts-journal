cd7e0d2bc42ae7261249348ed93e064655768c26
Andrii Yasnohorodskyi <yasnogorodskiy@gmail.com>
2023-03-20 09:49:03 +0000

    add app

---
+++ src/types/Emoji.ts
@@ -0,0 +1,17 @@
+type Emoji =
+  | "confused"
+  | "cool"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heart-eyes"
+  | "laugh"
+  | "sads"
+  | "shocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emoji;
